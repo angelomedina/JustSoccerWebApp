f6b8db64c3086d1e9210767690fff3f2a2ee2920
Wilfred <wilcr20@gmail.com>
2018-12-11 14:36:28 -0600

    GitIgnore

--- src/app/services/notas.service.ts
+++ src/app/services/notas.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {AngularFireDatabase,AngularFireList} from 'angularfire2/database'
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotasService {
 
-  constructor() { }
+  constructor(private firebase:AngularFireDatabase) { }
+
+  notesList:AngularFireList<any>;
+
+  getNotes(){ // Obtiene todods las notas dela DB
+    return this.notesList= this.firebase.list('notes');
+  }
+
+  addNote(note){  // Realiza la inserción de una nota en la DB
+    this.notesList.push(note);
+  }
 }
